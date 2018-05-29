ccafc60d85e68e32775bdb2e11a065e8de0d4f09
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-29 15:47:36 +0300

    router / check isAuthorized

---
+++ src/router/index.ts
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import auth from '../store/modules/auth'
+import HelloWorld from '@/components/HelloWorld.vue'
+import Login from '@/components/Login.vue'
+
+Vue.use(Router);
+
+const main = {
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+};
+
+const login = {
+    path: '/login',
+    name: 'Login',
+    component: Login
+};
+
+const routes = [
+    main,
+    login
+];
+
+const router = new Router({
+    routes
+});
+
+router.beforeEach((to, from, next) => {
+
+    const isAuthorized: boolean = !!auth.state.user;
+
+    if (!isAuthorized && to.name !== 'Login') {
+
+        return next('/login');
+
+    }
+
+    next();
+
+});
+
+export default router
