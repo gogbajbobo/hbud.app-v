d7b53df986d58710f8eb68ac1863b923737213d9
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-29 13:26:49 +0300

    components/Login.vue

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<script>
+
+    export default {
+        name: "Login"
+    }
+
+</script>
+
+<template>
+
+    <div>
+        <h1>Login</h1>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
