10e0bb9464d26f2b1c145951cf1b1afe0c7c30f7
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-29 11:24:46 +0300

    format

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  render: (h) => h(App)/*,
-  components: { App },
-  template: '<App/>'*/
-})
+    el: '#app',
+    router,
+    render: (h) => h(App)
+});
