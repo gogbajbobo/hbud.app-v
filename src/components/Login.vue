4be393ef09f5dbd2c3fcad386fb960b8c80879e7
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-30 15:15:47 +0300

    networkService.login()

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,22 +1,21 @@
 <script>
 
+    import networkService from '../services/network.service'
+
     export default {
 
         name: "Login",
 
         data() {
             return {
-                visible: false,
-                formLabelAlign: {
-                    username: '',
-                    password: ''
-                }
+                username: '',
+                password: ''
             }
         },
 
         methods: {
             onSubmit() {
-                console.log('submit!');
+                networkService.login(this.username, this.password)
             }
         }
 
@@ -30,14 +29,14 @@
 
         <h1>Login</h1>
 
-        <el-form label-position="left" label-width="100px" :model="formLabelAlign">
+        <el-form label-position="left" label-width="100px">
 
             <el-form-item label="Username">
-                <el-input v-model="formLabelAlign.username" placeholder="Please enter username"></el-input>
+                <el-input v-model="username" placeholder="Please enter username"></el-input>
             </el-form-item>
 
             <el-form-item label="Password">
-                <el-input v-model="formLabelAlign.password" type="password" placeholder="Please enter username"></el-input>
+                <el-input v-model="password" type="password" placeholder="Please enter username"></el-input>
             </el-form-item>
 
             <el-form-item>
