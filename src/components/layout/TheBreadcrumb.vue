09748e194ee760e106559b4b40a79a94644b40d7
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-31 17:37:37 +0300

    components/layout/TheBreadcrumb.vue

---
+++ src/components/layout/TheBreadcrumb.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+
+    import Vue from 'vue'
+    import router from '../../router'
+
+    export default Vue.extend({
+
+        name: "TheBreadcrumb",
+        data() {
+            return {
+                currentRoute: router.currentRoute
+            }
+        },
+        watch:{
+            $route (to, from){
+                this.currentRoute = to
+            }
+        }
+
+    })
+
+</script>
+
+<template>
+
+    <div class="el-breadcrumb">
+        <el-breadcrumb v-if="currentRoute.name !== 'Main'" separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ name: 'Main' }">homepage</el-breadcrumb-item>
+            <el-breadcrumb-item>{{ currentRoute.meta.localname }}</el-breadcrumb-item>
+        </el-breadcrumb>
+    </div>
+
+</template>
+
+<style scoped>
+
+    .el-breadcrumb {
+        height: 15px;
+    }
+
+</style>
