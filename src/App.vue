7f5430f51e03a2803d0985b9417a5c65cfc6c295
gogbajbobo <grigoriev.maxim@gmail.com>
2018-05-31 10:51:50 +0300

    hide aside if not authorized

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,21 @@
 
     import Vue from 'vue'
 
+    import auth from './store/modules/auth'
+
     import { TheHeader, TheAside, TheMain, TheFooter } from './components/layout/'
 
     export default Vue.extend({
         name: 'App',
-        components: { TheHeader, TheAside, TheMain, TheFooter }
+
+        components: { TheHeader, TheAside, TheMain, TheFooter },
+
+        computed: {
+            isAuthorized(): boolean {
+                return !!auth.state.user
+            }
+        }
+
     })
 
 </script>
@@ -19,7 +29,7 @@
             <el-header>
                 <the-header></the-header>
             </el-header>
-            <el-aside>
+            <el-aside v-if="isAuthorized">
                 <the-aside></the-aside>
             </el-aside>
             <el-main>
