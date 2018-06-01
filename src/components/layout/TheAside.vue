f2e547e0c3c9e65e757a907f0e4b2c2c9c03e40e
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-01 09:01:54 +0300

    menu in aside

--- src/components/layout/TheAside.vue
+++ src/components/layout/TheAside.vue
@@ -3,7 +3,15 @@
     import Vue from 'vue'
 
     export default Vue.extend({
-        name: "TheAside"
+
+        name: "TheAside",
+
+        methods: {
+            handleSelect(key, keyPath) {
+                console.log(key, keyPath);
+            }
+        }
+
     })
 
 </script>
@@ -11,15 +19,48 @@
 <template>
 
     <div>
-        <h4>The Aside</h4>
+
+        <el-menu @select="handleSelect">
+            <el-menu-item index="1">
+                <i class="el-icon-tickets"></i>
+                <span>Transactions</span>
+            </el-menu-item>
+            <el-submenu index="2">
+                <template slot="title">
+                    <i class="el-icon-goods"></i>
+                    <span>Accounts</span>
+                </template>
+                <el-menu-item index="2-1">
+                    <i class="el-icon-menu el-icon--green"></i>
+                    <span>Income</span>
+                </el-menu-item>
+                <el-menu-item index="2-2">
+                    <i class="el-icon-menu el-icon--blue"></i>
+                    <span>Active</span>
+                </el-menu-item>
+                <el-menu-item index="2-3">
+                    <i class="el-icon-menu el-icon--red"></i>
+                    <span>Exspense</span>
+                </el-menu-item>
+            </el-submenu>
+        </el-menu>
+
     </div>
 
 </template>
 
 <style scoped>
 
-    div {
-        background-color: blue;
+    .el-icon--green {
+        color: green !important;
+    }
+
+    .el-icon--blue {
+        color: blue !important;
+    }
+
+    .el-icon--red {
+        color: red !important;
     }
 
 </style>
