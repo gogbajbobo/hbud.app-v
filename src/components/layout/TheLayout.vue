050b21ba120efc4e8172aa8665f71f6c57ffdac5
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-03 14:08:55 +0300

    layout style for mobile

--- src/components/layout/TheLayout.vue
+++ src/components/layout/TheLayout.vue
@@ -80,12 +80,41 @@
     }
 
     @media (max-width: 768px) {
+
+        .el-header {
+            position: fixed;
+            top: 0;
+            width: 100%;
+            height: 60px;
+        }
+
         .el-aside {
+            position: fixed;
+            top: 60px;
+            bottom: 60px;
             max-width: 70px;
+            overflow-y: scroll;
         }
+
         .el-main {
-            left: 70px;
+            position: relative;
+            z-index: -99;
+            left: 0;
+            top: 30px;
+            bottom: 0;
+            right: 0;
+            margin-left: 70px;
+            margin-bottom: 60px;
+            overflow-y: scroll;
         }
+
+        .el-footer {
+            position: fixed;
+            height: 60px;
+            bottom: 0;
+            width: 100%;
+        }
+
     }
 
 </style>
