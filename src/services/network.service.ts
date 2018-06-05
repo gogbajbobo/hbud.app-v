52c8747db1f7f5ab88498ba9d73c5d1917abd353
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-05 16:46:38 +0300

    add request interceptor

--- src/services/network.service.ts
+++ src/services/network.service.ts
@@ -1,9 +1,44 @@
-import axios, {AxiosPromise} from 'axios'
+import axios from 'axios'
 import auth from '../store/modules/auth'
+import TokenService from "./token.service";
+
+const loginUrl = 'login';
+const exchangeTokenUrl = 'token';
 
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = 'http://maxbook.local:8002';
+
+axiosInstance.interceptors.request.use(config => {
+
+    console.log('send request', config.method, config.url);
+
+    switch (config.url) {
+
+        case loginUrl: { return config }
+
+        case exchangeTokenUrl: { return authorizedConfig(config) }
+
+        default: {
+
+            return TokenService.checkLifetime()
+                .then(() => authorizedConfig(config))
+                .catch(error => Promise.reject(error))
+
+        }
+
+    }
+
+}, error => Promise.reject(error));
+
+function authorizedConfig(config) {
+
+    const accessToken = auth.state.accessToken;
+    if (accessToken) config.headers.Authorization = `Bearer ${accessToken}`;
+
+    return config
+
+}
 
 class NetworkService {
 
@@ -16,14 +51,14 @@ class NetworkService {
             userAgent
         };
 
-        return axiosInstance.post(`login`, data)
+        return axiosInstance.post(loginUrl, data)
             .then(response => auth.commitAuthorized(response.data))
 
     }
 
     static exchangeToken(): Promise<any> {
 
-        return axiosInstance.get(`token`)
+        return axiosInstance.get(exchangeTokenUrl)
             .then(response => auth.commitTokenRecieved(response.data))
 
     }
