afb54ae81ec49a084af9cffbda46dc0b68a92d6d
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-06 19:36:13 +0300

    show users in table

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -9,12 +9,24 @@
 
         data() {
             return {
-                busy: <boolean> false
+                busy: <boolean> false,
+                usersData: <Array<Object>> [],
+                tableFields: <Array<Object>> [
+                    {prop: 'id', label: 'Id', width: '80', fixed: true},
+                    {prop: 'username', label: 'Name', width: '512px'},
+                    {prop: 'role', label: 'Role', width: '512px'}
+                ]
             }
         },
 
         created() {
+
+            this.busy = true;
+
             NetworkService.getUsers()
+                .then(users => this.usersData = users)
+                .catch(err => this.$message.error(err.message))
+                .then(() => this.busy = false)
         }
 
     });
@@ -23,8 +35,25 @@
 
 <template>
 
-    <div v-loading="busy">
+    <div>
+
         <h1>Users</h1>
+
+        <el-table :data="usersData"
+                  :default-sort = "{prop: 'id', order: 'descending'}"
+                  v-loading="busy"
+                  height="250">
+
+            <el-table-column v-for="field in tableFields"
+                             :prop="field.prop"
+                             :label="field.label"
+                             :fixed="field.fixed"
+                             :key="field.prop"
+                             :width="field.width"
+                             sortable>
+            </el-table-column>
+
+        </el-table>
     </div>
 
 </template>
