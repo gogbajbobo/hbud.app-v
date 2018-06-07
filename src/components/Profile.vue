7bd4c87372e2c4ff96a41caa83427c76f6ff3610
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-07 09:16:16 +0300

    profileForm

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -10,7 +10,38 @@
 
         data() {
             return {
-                user: <UserModel> auth.state.user
+                user: <UserModel> auth.state.user,
+                editMode: <boolean> false,
+                profileForm: {
+                    username: <string> '',
+                    password: <string> ''
+                },
+                busy: <boolean> false,
+                rules: {
+                    username: [
+                        { required: true, message: 'Please input username', trigger: 'blur' }
+                    ],
+                    password: [
+                        { min: 1, message: 'Please enter password', trigger: 'blur' }
+                    ]
+                }
+
+            }
+        },
+
+        created() {
+            this.profileForm.username = this.user.username
+        },
+
+        methods: {
+            editButtonPressed() {
+                this.editMode = true
+            },
+            submitForm() {
+                console.log('submitForm')
+            },
+            cancelForm() {
+                this.editMode = false
             }
         }
 
@@ -21,13 +52,37 @@
 <template>
 
     <el-card>
+
         <div slot="header">
             <span class="el-card--username">{{ user.username }}</span>
-            <el-button type="text">Edit</el-button>
+            <el-button class="el-button--edit" type="text" v-if="!editMode" @click="editButtonPressed">Edit</el-button>
         </div>
-        <div v-for="(value, key) in user" :key="key">
-            {{ `${ key }: ${ key === 'exp' ? new Date(value * 1000) : value }` }}
-        </div>
+
+        <template v-if="editMode">
+            <el-form v-loading="busy" @keyup.enter.native="submitForm" :model="profileForm" :rules="rules" ref="profileForm">
+
+                <el-form-item prop="username" label="You can change your username">
+                    <el-input v-model="profileForm.username" placeholder="Username"></el-input>
+                </el-form-item>
+
+                <el-form-item prop="password" label="… and password">
+                    <el-input v-model="profileForm.password" type="password" placeholder="Password"></el-input>
+                </el-form-item>
+
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm">Update profile</el-button>
+                    <el-button @click="cancelForm">Cancel</el-button>
+                </el-form-item>
+
+            </el-form>
+        </template>
+
+        <template v-else>
+            <div v-for="(value, key) in user" :key="key">
+                {{ `${ key }: ${ key === 'exp' ? new Date(value * 1000) : value }` }}
+            </div>
+        </template>
+
     </el-card>
 
 </template>
@@ -40,7 +95,7 @@
 
     }
 
-    .el-button {
+    .el-button--edit {
         float: right;
         padding: 3px 0;
     }
