3803f5ccc43db0c174daa790e6bd6aab1385dd8f
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-07 08:56:51 +0300

    components/Profile.vue show users profile data

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,18 +2,47 @@
 
     import Vue from 'vue'
 
+    import auth, { UserModel } from '../store/modules/auth'
+
     export default Vue.extend({
-        name: "Profile"
+
+        name: "Profile",
+
+        data() {
+            return {
+                user: <UserModel> auth.state.user
+            }
+        }
+
     })
 
 </script>
 
 <template>
 
-    <h1>Profile</h1>
+    <el-card>
+        <div slot="header">
+            <span class="el-card--username">{{ user.username }}</span>
+            <el-button type="text">Edit</el-button>
+        </div>
+        <div v-for="(value, key) in user" :key="key">
+            {{ `${ key }: ${ key === 'exp' ? new Date(value * 1000) : value }` }}
+        </div>
+    </el-card>
 
 </template>
 
 <style scoped>
+
+    .el-card--username {
+        font-size: large;
+        font-weight: bold;
+
+    }
+
+    .el-button {
+        float: right;
+        padding: 3px 0;
+    }
 
 </style>
