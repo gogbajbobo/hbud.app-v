9b74138e92cd56e0d5e25d71a7d62a762e98f6c1
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-10 10:20:35 +0300

    change dirname for dist build

--- config/index.js
+++ config/index.js
@@ -38,10 +38,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../hbud.app-v.build/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../hbud.app-v.build'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
