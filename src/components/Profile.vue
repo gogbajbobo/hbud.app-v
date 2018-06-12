67b2f605ea8ece413651ea198f1c7778e805dd03
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-12 10:48:37 +0300

    delete button in components/Profile.vue

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -4,6 +4,7 @@
 
     import auth from '../store/modules/auth'
     import NetworkService from '../services/network.service'
+    import MessageService from '../services/message.service'
     import User, { UserModel } from '../models'
 
     export default Vue.extend({
@@ -12,7 +13,7 @@
 
         data() {
             return {
-                user: <UserModel> auth.state.user,
+                user: <UserModel> {},
                 editMode: <boolean> false,
                 profileForm: {
                     username: <string> '',
@@ -27,7 +28,8 @@
                         { min: 1, message: 'Please enter password', trigger: 'blur' }
                     ]
                 },
-                isAdmin: <boolean> false
+                isAdmin: <boolean> false,
+                isSelf: <boolean> false
 
             }
         },
@@ -44,7 +46,9 @@
         methods: {
             setupComponent() {
 
-                if (this.isAdmin && this.id && (this.id !== auth.state.user!.id)) {
+                this.isSelf = this.id === auth.state.user!.id;
+
+                if (this.isAdmin && this.id && !this.isSelf) {
 
                     NetworkService.getUserById(this.id)
                         .then(user => {
@@ -72,6 +76,11 @@
             editButtonPressed() {
                 this.editMode = this.isAdmin
             },
+            deleteButtonPressed() {
+                MessageService.showConfirmMessage('Delete confirmation', 'Are you sure to delete this user?')
+                    .then(() => console.log('yes'))
+                    .catch(() => console.log('no'))
+            },
             submitForm() {
                 console.log('submitForm')
             },
@@ -81,7 +90,7 @@
         },
 
         watch: {
-            $route (to, from) {
+            $route(to, from) {
                 if (from.params.id !== to.params.id) this.setupComponent()
             }
         }
@@ -92,44 +101,56 @@
 
 <template>
 
-    <el-card>
+    <div>
+        <el-card>
 
-        <div slot="header">
+            <div slot="header">
 
-            <span class="el-card--username">{{ user.username }}</span>
-            <el-button v-if="isAdmin && !editMode"
-                       class="el-button--edit"
-                       type="text"
-                       @click="editButtonPressed">Edit</el-button>
+                <span class="el-card--username">{{ user.username }}</span>
+                <el-button v-if="isAdmin && !editMode"
+                           class="el-button--edit"
+                           type="text"
+                           @click="editButtonPressed">
+                    Edit
+                </el-button>
 
-        </div>
-
-        <template v-if="editMode">
-            <el-form v-loading="busy" @keyup.enter.native="submitForm" :model="profileForm" :rules="rules" ref="profileForm">
-
-                <el-form-item prop="username" label="You can change username">
-                    <el-input v-model="profileForm.username" placeholder="Username"></el-input>
-                </el-form-item>
-
-                <el-form-item prop="password" label="… and password">
-                    <el-input v-model="profileForm.password" type="password" placeholder="Password"></el-input>
-                </el-form-item>
-
-                <el-form-item>
-                    <el-button type="primary" @click="submitForm">Update profile</el-button>
-                    <el-button @click="cancelForm">Cancel</el-button>
-                </el-form-item>
-
-            </el-form>
-        </template>
-
-        <template v-else>
-            <div v-for="(value, key) in user" :key="key">
-                {{ `${ key }: ${ key === 'exp' ? new Date(value * 1000) : value }` }}
             </div>
-        </template>
 
-    </el-card>
+            <template v-if="editMode">
+                <el-form v-loading="busy" @keyup.enter.native="submitForm" :model="profileForm" :rules="rules" ref="profileForm">
+
+                    <el-form-item prop="username" label="You can change username">
+                        <el-input v-model="profileForm.username" placeholder="Username"></el-input>
+                    </el-form-item>
+
+                    <el-form-item prop="password" label="… and password">
+                        <el-input v-model="profileForm.password" type="password" placeholder="Password"></el-input>
+                    </el-form-item>
+
+                    <el-form-item>
+                        <el-button type="primary" @click="submitForm">Update profile</el-button>
+                        <el-button @click="cancelForm">Cancel</el-button>
+                    </el-form-item>
+
+                </el-form>
+            </template>
+
+            <template v-else>
+                <div v-for="(value, key) in user" :key="key">
+                    {{ `${ key }: ${ key === 'exp' ? new Date(value * 1000) : value }` }}
+                </div>
+            </template>
+
+        </el-card>
+
+        <el-button v-if="isAdmin && !isSelf"
+                   type="danger"
+                   class="el-button--delete"
+                   @click="deleteButtonPressed">
+            Delete user
+        </el-button>
+
+    </div>
 
 </template>
 
@@ -144,6 +165,10 @@
     .el-button--edit {
         float: right;
         padding: 3px 0;
+    }
+
+    .el-button--delete {
+        margin: 20px 10px;
     }
 
 </style>
