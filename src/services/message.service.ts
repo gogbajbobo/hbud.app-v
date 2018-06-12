0161a64e9f36c2b540b34204bb0b95e49bf2fc7a
gogbajbobo <grigoriev.maxim@gmail.com>
2018-06-12 10:31:59 +0300

    services/message.service.ts

--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -1,9 +1,24 @@
+import MobileDetect from 'mobile-detect' // may be just use window.innerWidth
 import { Message } from 'element-ui'
+import { MessageBox as MessageBoxM } from 'mint-ui'
+import { MessageBox as MessageBoxD } from 'element-ui';
+import { ElMessageBoxOptions } from 'element-ui/lib'
+import { MessageBoxData } from "element-ui/types/message-box";
+
+const md = new MobileDetect(navigator.userAgent);
 
 class MessageService {
 
     static showError(err: Error) {
         Message.error(`${ err.name }: ${ err.message }`)
+    }
+
+    static showConfirmMessage(title: string, text: string, options: ElMessageBoxOptions): Promise<MessageBoxData> {
+
+        return md.phone()
+                ? MessageBoxM.confirm(text, title, options)
+                : MessageBoxD.confirm(text, title, options);
+
     }
 
 }
