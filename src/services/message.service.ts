7cb928edb01d5ec1ef762783a056233ff7e4600a
gogbajbobo <grigoriev.maxim@gmail.com>
2018-07-25 10:23:22 +0300

    log error on MessageService.showError

--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -1,16 +1,20 @@
 import MobileDetect from 'mobile-detect' // may be just use window.innerWidth
 import { Message } from 'element-ui'
 import { MessageBox as MessageBoxM } from 'mint-ui'
-import { MessageBox as MessageBoxD } from 'element-ui';
+import { MessageBox as MessageBoxD } from 'element-ui'
 import { ElMessageBoxOptions } from 'element-ui/lib'
-import { MessageBoxData } from "element-ui/types/message-box";
+import { MessageBoxData } from "element-ui/types/message-box"
+import LoggerService from './logger.service'
 
 const md = new MobileDetect(navigator.userAgent);
 
 class MessageService {
 
     static showError(err: Error) {
-        Message.error(`${ err.name }: ${ err.message }`)
+
+        Message.error(`${ err.name }: ${ err.message }`);
+        LoggerService.error(err.message)
+
     }
 
     static showConfirmMessage(title: string, text: string): Promise<MessageBoxData> {
