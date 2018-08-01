705ef4cb84ad78658481034c9f1b078d3ec6abf7
gogbajbobo <grigoriev.maxim@gmail.com>
2018-08-01 09:49:20 +0300

    card click open dialog

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -9,7 +9,38 @@
         data() {
             return {
                 isMobile: <boolean> (window.innerWidth <= 768),
-                cards: [{title: 'Incomes'}, {title: 'Transfers'}, {title: 'Expenses'}]
+                cards: [{title: 'Incomes'}, {title: 'Transfers'}, {title: 'Expenses'}],
+                transactionDialogVisible: <boolean> false,
+                transaction: {
+                    from: <string> '',
+                    to: <string> ''
+                }
+            }
+        },
+
+        methods: {
+            cardClick(card) {
+
+                switch (card.title) {
+                    case 'Incomes': {
+                        this.transaction.from = 'Income';
+                        this.transaction.to = 'Current';
+                        break;
+                    }
+                    case 'Transfers': {
+                        this.transaction.from = 'Current';
+                        this.transaction.to = 'Current';
+                        break;
+                    }
+                    case 'Expenses': {
+                        this.transaction.from = 'Current';
+                        this.transaction.to = 'Expense';
+                        break;
+                    }
+                }
+
+                this.transactionDialogVisible = true;
+                
             }
         }
 
@@ -24,9 +55,25 @@
 
         <el-row :gutter="12">
             <el-col :xs="24" :sm="8" :md="8" :lg="8" :xl="8" v-for="card in cards" :key="card.title">
-                <router-link to="/"><el-card :shadow="isMobile ? 'always' : 'hover'">{{ card.title }}</el-card></router-link>
+
+                <div @click="cardClick(card)">
+                    <el-card :shadow="isMobile ? 'always' : 'hover'">{{ card.title }}</el-card>
+                </div>
+
             </el-col>
         </el-row>
+
+        <el-dialog title="Transaction" :visible.sync="transactionDialogVisible" :append-to-body="true">
+
+            <span>{{ transaction.from }}</span>
+            <span>{{ transaction.to }}</span>
+
+            <span slot="footer" class="dialog-footer">
+                <el-button >Cancel</el-button>
+                <el-button type="primary">Done</el-button>
+            </span>
+
+        </el-dialog>
 
     </div>
 
